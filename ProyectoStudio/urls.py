69ccf3d9927a050a180bfa2b78f1e69d3e8a30d6
´Yuliya <ybundur@gmail.com>
2023-02-15 22:13:34 +0100

    vistas 15.02

--- ProyectoStudio/urls.py
+++ ProyectoStudio/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('appbeautystudio/', include('AppBeautyStudio.urls') ),
+    path('', include('AppBeautyStudio.urls') ),
 ]
