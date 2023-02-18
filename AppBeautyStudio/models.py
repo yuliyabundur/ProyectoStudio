9cb00064a36220af789494d6e70931412099f484
´Yuliya <ybundur@gmail.com>
2023-02-19 00:05:49 +0100

    bd

--- AppBeautyStudio/models.py
+++ AppBeautyStudio/models.py
@@ -7,6 +7,9 @@ class Especialista(models.Model):
     apellidos = models.CharField(max_length=40)
     profesion = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.nombre
+
 class Cliente(models.Model):
 
     nombre = models.CharField(max_length=40)
@@ -14,15 +17,24 @@ class Cliente(models.Model):
     telefono = models.IntegerField()
     email = models.EmailField()
 
+    def __str__(self):
+        return self.apellidos
+
 class Servicio(models.Model):
 
     servicio = models.CharField(max_length=50)
     precio = models.IntegerField()
+
+    def __str__(self):
+        return self.servicio
 
 class Cita(models.Model):
 
     apellidos_especialista = models.CharField(max_length=40)
     fecha_cita = models.DateField()
     reservado = models.BooleanField()
+
+    def __str__(self):
+        return self.apellidos_especialista
     
    
