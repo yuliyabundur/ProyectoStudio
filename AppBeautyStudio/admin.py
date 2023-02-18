79ca3f1503a65ed60f730c8bdcff3a56c996b7be
´Yuliya <ybundur@gmail.com>
2023-02-18 22:53:09 +0100

    admin register models

--- AppBeautyStudio/admin.py
+++ AppBeautyStudio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Especialistas)
+admin.site.register(Servicios)
+admin.site.register(Clientes)
+admin.site.register(Citas)
