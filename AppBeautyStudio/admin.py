ac4d2b6d11eb442419bff8f9a6fb1b91f47358b7
´Yuliya <ybundur@gmail.com>
2023-02-18 23:12:11 +0100

    admin clases

--- AppBeautyStudio/admin.py
+++ AppBeautyStudio/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Especialistas)
-admin.site.register(Servicios)
-admin.site.register(Clientes)
-admin.site.register(Citas)
+admin.site.register(Especialista)
+admin.site.register(Servicio)
+admin.site.register(Cliente)
+admin.site.register(Cita)
--- AppBeautyStudio/models.py
+++ AppBeautyStudio/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Especialistas(models.Model):
+class Especialista(models.Model):
 
     nombre = models.CharField(max_length=40)
     apellidos = models.CharField(max_length=40)
     profesion = models.CharField(max_length=30)
 
-class Clientes(models.Model):
+class Cliente(models.Model):
 
     nombre = models.CharField(max_length=40)
     apellidos = models.CharField(max_length=40)
     telefono = models.IntegerField()
     email = models.EmailField()
 
-class Servicios(models.Model):
+class Servicio(models.Model):
 
     servicio = models.CharField(max_length=50)
     precio = models.IntegerField()
 
-class Citas(models.Model):
+class Cita(models.Model):
 
     apellidos_especialista = models.CharField(max_length=40)
     fecha_cita = models.DateField()
