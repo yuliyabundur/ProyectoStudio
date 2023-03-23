7132c8f1c3d2190369cc57a1b173f3138e2d168a
´Yuliya <ybundur@gmail.com>
2023-03-23 20:30:10 +0100

    tests

--- AppBeautyStudio/tests.py
+++ AppBeautyStudio/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from AppBeautyStudio.models import Especialista
 
 # Create your tests here.
+class URLTest(TestCase):
+
+    def test_inicio(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.especialista1 = Especialista.objects.create(
+            nombre = 'Especialista de ejemplo 1',
+            apellidos = 'Apellido de ejemplo 1',
+            profesion = 'Alguna profesion'
+        )
+
+    def test_modulo(self):
+        self.assertEquals(self.especialista1.nombre, 'Especialista de ejemplo 1')
+        self.assertEquals(self.especialista1.profesion, 'Alguna profesion')
